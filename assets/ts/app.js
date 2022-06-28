491c74baa6c361ca138bd7a3a0741cdf344c25d3
Felipe Mello <felipesilva.mello27@gmail.com>
2022-06-28 00:12:21 -0300

    Agr caso o aluno vá para exame, mostra a nota minima para passar

--- assets/ts/app.js
+++ assets/ts/app.js
@@ -3,81 +3,79 @@ let button = document.getElementById('button');
 let input1 = document.getElementById('input1');
 let input2 = document.getElementById('input2');
 let boxResultado = document.getElementById('resultado');
+
 let buttonExame = document.getElementById('buttonExame');
 let input1Exame = document.getElementById('input1Exame');
 let input2Exame = document.getElementById('input2Exame');
 let boxResultadoExame = document.getElementById('resultadoExame');
-function nota(numero1, numero2) {
-    let aprovado = ' Aprovado';
-    let exame = ' Exame';
+
+function notaMD(numero1, numero2) {
+    let aprovado = 'Aprovado';
+    let exame = 'Exame';
     let soma = ((numero1 * 9) + (1 * numero2)) / 10;
-    let resultado = arredondaNota(soma);
+    let resultado = arredondaNotaMD(soma);
     if (resultado >= 6) {
-        boxResultado.innerHTML = String(resultado + aprovado);
+        if (resultado != soma) {
+            boxResultado.innerHTML = String(`Nota: ${soma.toFixed(3)} \nNota arredondada: ${resultado.toFixed(3)} \nResultado: ${aprovado}`);
+        }
+        else {
+            boxResultado.innerHTML = String(`Nota: ${resultado.toFixed(3)} - ${aprovado}`);
+        }
     }
     else {
-        boxResultado.innerHTML = String(resultado + exame);
-        input1Exame.value = String(resultado);
+        let notaMininaMF = 9.5 - resultado;
+        boxResultado.innerHTML = String(`${resultado.toFixed(3)} \nResultado: ${exame} \nNota minima MF: ${notaMininaMF}`);
+        input1Exame.value = String(resultado.toFixed(3));
     }
 }
-function notaFinal(numero1, numero2) {
+
+function notaMF(numero1, numero2) {
     let soma = (numero1 + numero2) / 2;
-    let resultadoFinal = arredondaNota(soma);
-    let aprovado = ' Aprovado';
-    let reprovado = ' Reprovado';
-    if (resultadoFinal < 5) {
-        boxResultadoExame.innerHTML = String(resultadoFinal + reprovado);
+    let resultadoFinal = arredondaNotaMF(soma);
+    let aprovado = 'Aprovado';
+    let reprovado = 'Reprovado';
+    if (resultadoFinal >= 5) {
+        if (resultadoFinal != soma) {
+            boxResultadoExame.innerHTML = String(`Nota: ${soma.toFixed(3)} \nNota arredondada: ${resultadoFinal.toFixed(3)} \nResultado: ${aprovado}`);
+        }
+        else {
+            boxResultadoExame.innerHTML = String(`Nota: ${resultadoFinal.toFixed(3)} \n${aprovado}`);
+        }
     }
     else {
-        boxResultadoExame.innerHTML = String(resultadoFinal + aprovado);
+        boxResultadoExame.innerHTML = String(`${resultadoFinal.toFixed(3)} + ${reprovado}`);
     }
 }
-function arredondaNota(nota) {
-    if (nota >= 0.75 && nota < 1) {
-        return 1;
-    }
-    else if (nota >= 1.75 && nota < 2) {
-        return 2;
-    }
-    else if (nota >= 2.75 && nota < 3) {
-        return 3;
-    }
-    else if (nota >= 3.75 && nota < 4) {
-        return 4;
-    }
-    else if (nota >= 4.75 && nota < 5) {
-        return 5;
-    }
-    else if (nota >= 5.75 && nota < 6) {
+
+function arredondaNotaMD(nota) {
+    if (nota >= 5.7 && nota < 6) {
         return 6;
-    }
-    else if (nota >= 6.75 && nota < 7) {
-        return 7;
-    }
-    else if (nota >= 7.75 && nota < 8) {
-        return 8;
-    }
-    else if (nota >= 8.75 && nota < 9) {
-        return 9;
-    }
-    else if (nota >= 9.75 && nota < 10) {
-        return 10;
     }
     else {
         return nota;
     }
 }
+
+function arredondaNotaMF(nota) {
+    if (nota >= 4.75 && nota < 5) {
+        return 5;
+    }
+    else {
+        return nota;
+    }
+}
+
 if (button) {
     button.addEventListener('click', () => {
         if (input1 && input2) {
-            console.log(nota(Number(input1.value), Number(input2.value)));
+            notaMD(Number(input1.value), Number(input2.value));
         }
     });
 }
 if (buttonExame) {
     buttonExame.addEventListener('click', () => {
         if (input1Exame && input2Exame) {
-            console.log(notaFinal(Number(input1Exame.value), Number(input2Exame.value)));
+            notaMF(Number(input1Exame.value), Number(input2Exame.value));
         }
     });
 }
